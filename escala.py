02ed4a1fec18403950f715c5eafe05593c9eca66
Vinicius Lima <vibylima75@gmail.com>
2026-05-05 07:54:09 +0000

    Implementação da função calcular_status

--- escala.py
+++ escala.py
@@ -4,19 +4,25 @@ from datetime import datetime
 
 def converter_data(data_texto):    #Receber uma data em texto e converter para data
     try:
-        data_texto = datetime.strptime(data_texto, "%d/%m/%Y").date()
-        return data_texto
+        data_convertida = datetime.strptime(data_texto, "%d/%m/%Y").date()
+        return data_convertida
 
     except ValueError:
         return None
 
 
-def calcular_status():
-    pass
+def calcular_status(data_inicio, data_consulta, dias_trabalho, dias_folga):
+    ciclo = dias_trabalho + dias_folga
+
+    dias_passados = (data_consulta - data_inicio).days
+
+    posicao_ciclo = dias_passados % ciclo
+    
+    if posicao_ciclo < dias_trabalho:
+        return "Trabalhando"
+    else:
+        return "Folga"
+
 
 def gerar_proximos_dias():
     pass
-
-print(converter_data("05/05/2026"))
-print(converter_data("2026-05-05"))
-print(converter_data("abc"))
