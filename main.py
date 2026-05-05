39536f910afdc57a61ccaecd66996d4ece73ee50
Vinicius Lima <vibylima75@gmail.com>
2026-05-05 06:30:20 +0000

    Estrutura inicial do projeto

---
+++ main.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+print("==== SIMULADOR DE ESCALAS ====")
+print("\n1 - Consultar uma data.")
+print("2 - Ver proximas dias.")
+print("3 - Alterar escalas.")
+print("4 - Sair.")
+
+while True:
+    menu = input("\nEscolha uma opção: ")
+    
+    if menu == "1":
+        pass
+    
+    elif menu == "2":
+        pass
+    
+    elif menu == "3":
+        pass
+
+    elif menu == "4":
+        print("Volte sempre")
+        break
+    
+    else:
+        print("Favor inserir uma opção valida entre 1 e 4!")
+        continue
