814a7d39944343775fb9589a0b82d46b49260c09
Vinicius Lima <vibylima75@gmail.com>
2026-05-07 18:02:25 -0300

    feat: adiciona lógica inicial de cálculo da escala

--- escala.py
+++ escala.py
@@ -1,9 +1,9 @@
 #Responsavel pela logica da escala.
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def converter_data(data_texto):    #Receber uma data em texto e converter para data
-    try:
+    try:                                                                                #Tenta converter a data usando o formato dia/mês/ano
         data_convertida = datetime.strptime(data_texto, "%d/%m/%Y").date()
         return data_convertida
 
@@ -24,5 +24,19 @@ def calcular_status(data_inicio, data_consulta, dias_trabalho, dias_folga):
         return "Folga"
 
 
-def gerar_proximos_dias():
-    pass
+def gerar_proximos_dias(data_inicio, quantidade_dias, dias_trabalho, dias_folga):          #Gerar os próximos dias de trabalho e folga a partir da data atual
+    for i in range(quantidade_dias):
+        data_atual =data_inicio + timedelta(days=i)
+
+        status = calcular_status(
+            data_inicio,
+            data_atual,
+            dias_trabalho,
+            dias_folga
+        )
+
+        print(f"{data_atual.strftime('%d/%m/%Y')}: {status}")
+
+data_inicio = converter_data("01/05/2026")
+
+gerar_proximos_dias(data_inicio, 15, 6, 3)
