94a359518ba26779f8ed633c223d262a5454c960
Vinicius Lima <vibylima75@gmail.com>
2026-05-25 09:05:24 -0300

    feat: adiciona função para salvar escalas em JSON

--- armazenamento.py
+++ armazenamento.py
@@ -1,16 +1,20 @@
 import json
 
+CAMINHO_ESCALAS = "data/escalas.json"
+
 def carregar_escalas():
     try:
-        with open("data/escalas.json", "r", encoding="utf-8") as file:
+        with open(CAMINHO_ESCALAS, "r", encoding="utf-8") as file:
             escalas = json.load(file)
             return escalas
     except FileNotFoundError:
         return []
 
+def salvar_escalas(escalas):
+    with open(CAMINHO_ESCALAS, "w", encoding="utf-8") as file:
+        json.dump(escalas, file, ensure_ascii=False, indent=4)
 
 if __name__ == "__main__":
-    from interface import exibir_escalas_salvas
-
     escalas = carregar_escalas()
-    exibir_escalas_salvas(escalas)
+    salvar_escalas(escalas)
+    print("Escalas salvas com sucesso.")
