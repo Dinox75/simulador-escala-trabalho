2e8c7604bce7d2529eb83908c8b397db8221523e
Vinicius Lima <vibylima75@gmail.com>
2026-05-27 10:36:36 -0300

    feat: adiciona nomes amigaveis para tipos de escala

--- tipos_escala.py
+++ tipos_escala.py
@@ -10,6 +10,14 @@ TIPOS_ESCALA_SUPORTADOS = [
     TIPO_TURNO_ROTATIVO
 ]
 
+NOMES_TIPOS_ESCALA = {
+    TIPO_CICLO_DIAS: "Ciclo por dias",
+    TIPO_CICLO_HORAS: "Ciclo por horas",
+    TIPO_TURNO_ROTATIVO: "Turno rotativo"
+}
 
 def validar_tipo_escala(tipo):
     return tipo in TIPOS_ESCALA_SUPORTADOS
+
+def obter_nome_tipo(tipo):
+    return NOMES_TIPOS_ESCALA.get(tipo, "Ciclo por dias")
